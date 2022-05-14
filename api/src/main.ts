932b64ac07aca8823157c2cbe3a2c4212301297e
Brahian Giraldo Medina <bgiraldo@gmstek.com>
2022-05-13 21:20:04 -0500

    added swagger to manage api documentation

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1'
+  });
+
+  const config = new DocumentBuilder()
+      .setTitle('GitHub Integration')
+      .setDescription('Create an integration with GitHub to consume some resources')
+      .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
